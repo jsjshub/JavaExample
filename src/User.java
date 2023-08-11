e9e825df0107fc66b23f86f041f3c5213a5c6e46
Hi <iamjsjs0319@gmail.com>
2023-08-11 23:24:55 +0900

    생성자 테스트

---
+++ src/User.java
@@ -0,0 +1,21 @@
+public class User {
+    private String email;
+    private String password;
+    private String name;
+
+
+    public User(String email, String password, String name) {
+        this.email = email;
+        this.password = password;
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/UserExample01.java
@@ -0,0 +1,8 @@
+public class UserExample01 {
+    public static void main(String[] args) {
+        User user = new User("test@test.com", "1234@", "kim");
+        System.out.println("user = " + user);
+
+
+    }
+}
