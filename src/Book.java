bc59c108b626f30f3af857393513f77bb7700bc2
Hi <iamjsjs0319@gmail.com>
2023-08-11 23:15:11 +0900

    Getter Setter

---
+++ src/Book.java
@@ -0,0 +1,11 @@
+public class Book {
+    private int price;
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
---
+++ src/BookExample.java
@@ -0,0 +1,8 @@
+public class BookExample {
+    public static void main(String[] args) {
+        Book book = new Book();
+        book.setPrice(1000);
+
+        System.out.println(book.getPrice());
+    }
+}
